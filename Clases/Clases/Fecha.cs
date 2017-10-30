4fa7e38589ea0e9304f7efe65ab4a312d09b898e
Gabriel Arroyo Pajares <gabrielarroyo396@gmail.com>
2017-10-30 14:30:15 +0100

    Add files via upload

---
+++ Clases/Clases/Fecha.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Fecha
+    {
+        // determina si la fecha se corresponde año bisiesto
+        // aumenta el mes +1
+        // Crear 3 fechas y leer los valores de las 3 fechas por pantalla, Escriba mayor de todas,
+        int dia, mes, anio;
+        //Constructores
+
+        public Fecha()
+        {
+            dia = 1;
+            mes = 1;
+            anio = 1970;
+        }
+        public Fecha(int d, int m, int a)
+        {
+            dia = d;
+            mes = m;
+            anio = a;
+        }
+
+        public bool EsBisiesto(int a)
+        {
+            if (a % 4 == 0)
+                return true;
+            else
+                return false;
+
+        }
+
+
+    }
+}
---
+++ Clases/Clases/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fecha f1 = new Fecha();
+            Fecha f2 = new Fecha(12, 2, 2017);
+
+
+        }
+    }
+}
